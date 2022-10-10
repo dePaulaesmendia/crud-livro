76e93661538f55fbc7360a9b684f2a75bdd584ae
dePaulaesmendia <jack.esmendia@gmail.com>
2022-10-10 19:34:06 -0300

    inicio projeto

---
+++ src/app/components/listar-livro/listar-livro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-livro',
+  templateUrl: './listar-livro.component.html',
+  styleUrls: ['./listar-livro.component.scss']
+})
+export class ListarLivroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
